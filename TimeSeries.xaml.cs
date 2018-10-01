a510660e8c6deb00ab1c268a064d53ced789f045
theQuantumMechanic <albertferguson118@gmail.com>
2018-10-01 14:55:58 +1000

    C# classes and XAML with App entry point.
    WPF and LiveCharts for visualisation of dataset when in windowed mode.

---
+++ TimeSeries.xaml.cs
@@ -0,0 +1,41 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace MachineLearnerWPF
+{
+    /// <summary>
+    /// Interaction logic for TimeSeries.xaml
+    /// </summary>
+    public partial class TimeSeries : UserControl
+    {
+        public TimeSeries()
+        {
+            InitializeComponent();
+            Constants.RawData data = Constants.Item.GetRawData();
+            int n = Constants.Item.GetCount();
+            var Ewma = DataPreparer.Weighter(data, 0.5);
+
+            YFormatter = value => value.ToString("C");
+
+            List<double> BuyQ = data.BuyQuantity.Select<int, double>(i => i).ToList();
+            List<double> BuyP = data.BuyPrice.Select<int, double>(i => i).ToList();
+            List<double> SellQ = data.SellQuantity.Select<int, double>(i => i).ToList();
+            List<double> SellP = data.SellPrice.Select<int, double>(i => i).ToList();
+
+            for (int i = 0; i < BuyQ.Count(); i++)
+            {
+                EWMAPrice.Add(new ObservablePoint(i, Ewma._strdBuyPrice.ElementAt(i)));
+                Price.Add(new ObservablePoint(i, BuyP.ElementAt(i)));
+            }
+            DataContext = this;
+        }
+
+        public ChartValues<ObservablePoint> EWMAPrice { get; set; }
+        public ChartValues<ObservablePoint> Price { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+    }
+}
